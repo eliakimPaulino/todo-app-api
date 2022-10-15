28a5fa9ad4b997c9dbc7f7ef02f2f71c2be2f029
Eliakim Paulino França <eliakim.paulino@gmail.com>
2022-10-15 07:33:16 -0300

    Adicionado o Json

---
+++ lib/src/home_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('ToDo\'s List'),
+      ),
+      body: ListView.builder(
+        itemCount: 10,
+        itemBuilder: (context, index) {
+          return ListTile();
+        },
+      ),
+    );
+  }
+}
